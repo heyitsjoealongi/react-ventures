d5f8cfbe94145a4530658b8ae9ea3fd9ef3476f7
Joe Alongi <datavinivici@outlook.com>
2020-09-27 03:48:58 -0400

    Integrated Reactstrap & Bootstrap
    Added packages for React-based Bootstrap components and Bootstrap styling. Imported Zeit custom css modules for allowing Bootstrap imports.

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,38 @@
 import Head from 'next/head'
 
+import Example from '../components/Card'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container my-5 py-5">
       <Head>
-        <title>Create Next App</title>
+        <title>React Ventures</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+          Welcome to <a href="https://nextjs.org">React Ventures</a>
+      </h1>
 
-        <p className="description">
+      <p className="description">
           Get started by editing <code>pages/index.js</code>
-        </p>
+      </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-4">
+              <Example />
+            </div>
+            <div class="col-sm-4">
+              <Example />
+            </div>
+            <div class="col-sm-4">
+              <Example />
+            </div>
+          </div>
         </div>
       </main>
 
