6c54643e6063b9e37a94f2ec4ff25a3056009876
Joe Alongi <datavinivici@outlook.com>
2020-10-03 16:28:09 -0400

    Team Page
    Created team page with simple link for button in loop of site as placeholder.
    
    Team Page
    
    Additional columns normalize to float properly and removed css class.

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
-import Navigation from '../components/Navigation'
+import Navigation from '../components/Navigation';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../styles/utilities.css';
--- pages/team.js
+++ pages/team.js
@@ -1,7 +1,9 @@
+import Link from 'next/link'
+
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
-import Navigation from '../components/Navigation'
+import Navigation from '../components/Navigation';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../styles/utilities.css';
@@ -9,21 +11,31 @@ import '../styles/utilities.css';
 export default function Team() {
   return (
     <>
-    
+
       <Navigation />
       
       <main>
-        <Container className="py-5 my-5">
+          <Container className="py-5 my-5">
 
             <Row noGutters className="v-align">
 
-              <Col xs={{ order: 1 }} md={{size: 12}}>
+              <Col md={{size: 7}} className="negative-offsets-n2">
+                <img className="py-1" src="http://via.placeholder.com/150x150" alt=""></img>
+                <h2 className="display-4 py-2">team</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="py-2 hover-ing">
+                  <Link href="/">
+                    <a target="_blank">
+                      🦄 <span>website.com</span>
+                    </a>
+                  </Link>
+                </div>
               </Col>
 
             </Row>
 
-        </Container>
-      </main>
+          </Container>
+        </main>
 
     </>
   )
