d3119473c74bba097ae16918a79ab14eb289844e
Joe Alongi <datavinivici@outlook.com>
2020-10-11 12:09:45 -0400

    Package Upgrades & README Updates
    Updated packages to --latest and imported variables. Edited README.

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -1,83 +1,91 @@
 /* Framework */
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 /* Bootstrap Components */
-import {Card, CardImg, CardBody,CardTitle, CardSubtitle, Button, Col} from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Col,
+} from "reactstrap";
 
 class PortfolioItem extends React.Component {
-    constructor(props) {
-        super(props);
-       this.state ={
-           projects :[
-               {
-                image:"/brand-placeholder.png",
-                title:"Project One",
-                subtitle:"Venture",
-                button:"/portfolio/project-one"
-               },
-               {
-                image:"/brand-placeholder.png",
-                title:"Project Two",
-                subtitle:"Venture",
-                button:"/portfolio/project-one"
-               },
-               {
-                image:"/brand-placeholder.png",
-                title:"Project Three",
-                subtitle:"Venture",
-                button:"/portfolio/project-one"
-               },
-               {
-                image:"/brand-placeholder.png",
-                title:"Project Four",
-                subtitle:"Venture",
-                button:"/portfolio/project-one"
-               },
-               {
-                image:"/brand-placeholder.png",
-                title:"Project Five",
-                subtitle:"Venture",
-                button:"/portfolio/project-one"
-               },
-               {
-                image:"/brand-placeholder.png",
-                title:"Project Six",
-                subtitle:"Venture",
-                button:"/portfolio/project-one"
-               }
-           ]
-       }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [
+        {
+          image: "/brand-placeholder.png",
+          title: "Project One",
+          subtitle: "Venture",
+          button: "/portfolio/project-one",
+        },
+        {
+          image: "/brand-placeholder.png",
+          title: "Project Two",
+          subtitle: "Venture",
+          button: "/portfolio/project-one",
+        },
+        {
+          image: "/brand-placeholder.png",
+          title: "Project Three",
+          subtitle: "Venture",
+          button: "/portfolio/project-one",
+        },
+        {
+          image: "/brand-placeholder.png",
+          title: "Project Four",
+          subtitle: "Venture",
+          button: "/portfolio/project-one",
+        },
+        {
+          image: "/brand-placeholder.png",
+          title: "Project Five",
+          subtitle: "Venture",
+          button: "/portfolio/project-one",
+        },
+        {
+          image: "/brand-placeholder.png",
+          title: "Project Six",
+          subtitle: "Venture",
+          button: "/portfolio/project-one",
+        },
+      ],
+    };
+  }
 
-    render() {
-        const {projects} = this.state;
+  render() {
+    const { projects } = this.state;
 
-        return (
-          <>
-            {projects.map((data) => (
-              <Link href={data.button}>
-                <Col lg={{size: 4}}>
-                  <Card className="cards">
-                    <CardImg top width="100%" src={data.image} alt={data.title} />
-                    <CardBody>
-                      <CardTitle>
-                        <h4>{data.title}</h4>
-                      </CardTitle>
-                      <CardSubtitle className="my-3 sub-one">
-                        <span>{data.subtitle}</span>
-                      </CardSubtitle>
-                      <Button className="clicking">
-                        <span>See More</span>
-                      </Button>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Link>
-            ))}
-          </>
-        );
-    }
+    return (
+      <>
+        {projects.map((data) => (
+          <Link href={data.button}>
+            <Col lg={{ size: 4 }}>
+              <Card className="cards">
+                <CardImg top width="100%" src={data.image} alt={data.title} />
+                <CardBody>
+                  <CardTitle>
+                    <h4>{data.title}</h4>
+                  </CardTitle>
+                  <CardSubtitle className="my-3 sub-one">
+                    <span>{data.subtitle}</span>
+                  </CardSubtitle>
+                  <Button className="clicking">
+                    <span>See More</span>
+                  </Button>
+                </CardBody>
+              </Card>
+            </Col>
+          </Link>
+        ))}
+      </>
+    );
+  }
 }
 
 export default PortfolioItem;
--- pages/team.js
+++ pages/team.js
@@ -1,40 +1,42 @@
 /* Framework */
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 /* Bootstrap Components */
-import {Container, Row, Col} from 'reactstrap';
+import { Container, Row, Col } from "reactstrap";
 
 /* Components */
-import Navigation from '../components/Navigation';
-import ContactButton from '../components/ContactButton';
+import Navigation from "../components/Navigation";
+import ContactButton from "../components/ContactButton";
 
 /* Styles */
-import '../styles/theme.css';
-import '../styles/utilities.css';
+import "../styles/theme.css";
+import "../styles/utilities.css";
 
 export default function Team() {
   return (
     <>
-
       <Navigation />
-      
+
       <main>
-          <Container className="py-5 my-5">
-
-            <Row noGutters>
-
-              <Col lg={{size: 9}}>
-                <h2 className="display-4 py-2">🦄 Team</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <ContactButton />
-              </Col>
-
-            </Row>
-
-          </Container>
-        </main>
-
+        <Container className="py-5 my-5">
+          <Row noGutters>
+            <Col lg={{ size: 9 }}>
+              <h2 className="display-4 py-2">🦄 Team</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <ContactButton />
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </>
-  )
+  );
 }
