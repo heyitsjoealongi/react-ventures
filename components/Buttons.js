18a1df54290b60ac1ebf4b147e6b0631c068256a
Joe Alongi <collectedview@gmail.com>
2020-12-13 16:16:14 -0500

    Home
    Developed homepage for React Ventures content to build outwards as an application.

---
+++ components/Buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const Buttons = (props) => {
+  return (
+    <div>
+      <Button color="primary">primary</Button>{' '}
+    </div>
+  );
+}
+
+export default Buttons;
